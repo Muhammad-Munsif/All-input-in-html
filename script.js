aebee7f24054bd9eb1ac636dc82cea778925844f
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-10-06 22:05:28 +0500

    first commit added for css

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 // Getting Input Values
-document.getElementById("submitBtn").addEventListener("click", function() {
-    let name = document.getElementById("nameInput").value;
-    let email = document.getElementById("emailInput").value;
-    alert("Name: " + name + "\nEmail: " + email);
-  });
+document.getElementById("submitBtn").addEventListener("click", function () {
+  let name = document.getElementById("nameInput").value;
+  let email = document.getElementById("emailInput").value;
+  alert("Name: " + name + "\nEmail: " + email);
+});
 // Handling File Input
-  document.getElementById("fileInput").addEventListener("change", function() {
-    let file = this.files[0];
-    alert("Selected file: " + file.name);
-  });
+document.getElementById("fileInput").addEventListener("change", function () {
+  let file = this.files[0];
+  alert("Selected file: " + file.name);
+});
 // Validating an Email Input
-  document.getElementById("emailInput").addEventListener("input", function() {
-    let email = this.value;
-    if (!email.includes("@")) {
-      this.style.borderColor = "red";
-    } else {
-      this.style.borderColor = "green";
-    }
-  });
-  
+document.getElementById("emailInput").addEventListener("input", function () {
+  let email = this.value;
+  if (!email.includes("@")) {
+    this.style.borderColor = "red";
+  } else {
+    this.style.borderColor = "green";
+  }
+});
