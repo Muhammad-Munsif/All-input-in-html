7ef90828c21ee67664758280e3bbe68922b9b2cd
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-10-06 21:10:03 +0500

    first commit added

--- script.js
+++ script.js
@@ -4,13 +4,11 @@ document.getElementById("submitBtn").addEventListener("click", function() {
     let email = document.getElementById("emailInput").value;
     alert("Name: " + name + "\nEmail: " + email);
   });
-
 // Handling File Input
   document.getElementById("fileInput").addEventListener("change", function() {
     let file = this.files[0];
     alert("Selected file: " + file.name);
   });
-
 // Validating an Email Input
   document.getElementById("emailInput").addEventListener("input", function() {
     let email = this.value;
